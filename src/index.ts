974ac542338e924c89b53185eeb74f00eec7f6a3
Nan72 <lecongnhan29@gmail.com>
2025-02-14 11:31:15 +0700

    feat(camel): ref for easier tree shaking

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { clipboard, expose, HeadlessCommand, toast } from "@kksh/api/headless"
-import _ from "lodash"
+import camelCase from "lodash/camelcase"
 
 class CamelCaseExt extends HeadlessCommand {
 	async load() {
 		if (!await clipboard.hasText()) return;
 
 		const clipboardText = await clipboard.readText();
-		const convertedText = _.camelCase(clipboardText);
+		const convertedText = camelCase(clipboardText);
 
 		await clipboard.writeText(convertedText);
 		await toast.success(`Copied: "${convertedText}"`);
